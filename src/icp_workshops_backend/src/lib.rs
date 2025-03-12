153354d9d4c02a2c11659633dea74d6c9cfc44c2
Veganins <124815808+Veganins@users.noreply.github.com>
2025-03-12 19:13:50 +0100

    Update lib.rs remove token

--- src/icp_workshops_backend/src/lib.rs
+++ src/icp_workshops_backend/src/lib.rs
@@ -16,6 +16,7 @@ pub struct Response {
 
 #[ic_cdk::update]
 async fn translate() -> String {
+    let token = "";
     let arg = CanisterHttpRequestArgument {
         url: "https://api-inference.huggingface.co/models/google-t5/t5-base".to_string(),
         max_response_bytes: None,
@@ -23,7 +24,7 @@ async fn translate() -> String {
         headers: vec![
             HttpHeader {
                 name: "Authorization".to_string(),
-                value: "Bearer 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".to_string(),
+                value: format!("Bearer {}", token).to_string(),
             }
         ],
         body: Some(r#"{"inputs": "What's up?"}"#.into()),
